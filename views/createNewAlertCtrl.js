01fe06a1069da95ac656c3b953e72b37678fa70e
Gal Shani <galshani76@gmail.com>
2017-08-02 13:44:23 +0300

    working on the create new alert form
    saving the new create elemets on an object

--- views/createNewAlertCtrl.js
+++ views/createNewAlertCtrl.js
@@ -1,3 +1,11 @@
-alertsApp.controller("createNewAlertCtrl", function ($scope) {
+alertsApp.controller("createNewAlertCtrl", function ($scope,$http) {
     $scope.test = "TESTTTT";
-});
+
+$http.get("categories.json").then(function(response) {
+    $scope.categories = [];
+    $scope.newAlert = {};
+    for (var i = 0; i < response.data.length; i++) {
+      $scope.categories.push(new Category(response.data[i]));
+    }
+})
+  });
