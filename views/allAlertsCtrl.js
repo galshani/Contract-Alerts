250b5941bbea552f935f17dd6d767c460cb90025
Gal Shani <galshani76@gmail.com>
2017-08-08 09:39:01 +0300

    updating the allAlertsCtrl

--- views/allAlertsCtrl.js
+++ views/allAlertsCtrl.js
@@ -1,7 +1,14 @@
-alertsApp.controller("allAlertsCtrl", function ($scope, alerts) {
+alertsApp.controller("allAlertsCtrl", function ($scope, alerts,activeUser,alerts) {
     if (alerts.getAll().length === 0) {
        $scope.alertArr=[];
-       $http.get(activeUser.get())
+       $http.get(activeUser.get().contractSource).then(function(response) {
+            alerts.load(response.data);
+            $scope.alertArr = alerts.getAll();
+        });
+    } else {
+        $scope.recipeArr = recipes.getAll();
     }
 
-})
+    }
+
+)
