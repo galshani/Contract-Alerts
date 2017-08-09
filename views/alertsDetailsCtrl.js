00c79fb5bdc19ad96f65c529c5f05dd452fe786b
Gal Shani <galshani76@gmail.com>
2017-08-09 13:36:21 +0300

    commiting changes made to the update allAlerts

---
+++ views/alertsDetailsCtrl.js
@@ -0,0 +1,34 @@
+//recipeApp.controller("RecipeDetailsCtrl", function ($scope, $location, recipes, activeUser,  $routeParams, Recipe) {
+
+    // If the user is not logged in going back to home screen
+  //  if (!activeUser.isLoggedIn()) {
+   //     $location.path("/");
+    //    return;
+   // }
+
+    // Creating a copy of the recipe object so changes won't be reflected on the array
+//    $scope.recipe = new Recipe(recipes.get($routeParams.recipeIndex));
+
+ //   $scope.cancel = function() {
+  //      $location.path("/recipes");
+  //  }
+
+  //  $scope.update = function() {
+    //    recipes.update($routeParams.recipeIndex, $scope.recipe);
+   //     $location.path("/recipes");
+  //  }
+
+  //  $scope.remove = function() {
+   //     recipes.remove($routeParams.recipeIndex);
+   //     $location.path("/recipes");
+  //  }
+    
+//})
+alertsApp.controller("alertsDeatailsCtrl",function ($scope, $location, alerts, activeUser,  $routeParams, Alert){
+    if (!activeUser.isLoggedIn()) {
+      $location.path("/");
+    return;
+    }
+ $scope.alert = new Alert(alerts.get($routeParams.alertIndex));
+}
+)
--- views/allAlertsCtrl.js
+++ views/allAlertsCtrl.js
@@ -1,22 +1,4 @@
-alertsApp.controller("allAlertsCtrl", function ($scope, alerts, activeUser, $http) {
-
-    $scope.deleteItems = function () {
-        for (var i = 0; i < $scope.alertArr.length; i++) {
-            if ($scope.alertArr[i].isChecked) {
-                alerts.remove(i);
-            }
-            alertArr = alerts.getAll();
-        }
-    }
-    // to do- when update can create new using modal 
-    $scope.updateItem = function () {
-        for (var i = 0; i < $scope.alertArr.length; i++) {
-            if ($scope.alertArr[i].isChecked) {
-                alerts.update(i);
-            }
-            alertArr = alerts.getAll();
-        }
-    }
+alertsApp.controller("allAlertsCtrl", function ($scope, alerts, activeUser, $http, $location) {
     if (alerts.getAll().length === 0) {
         $scope.alertArr = [];
         $http.get(activeUser.get().contractSource).then(function (response) {
@@ -28,6 +10,27 @@ alertsApp.controller("allAlertsCtrl", function ($scope, alerts, activeUser, $htt
         $scope.alertArr = alerts.getAll();
     }
 
+    $scope.deleteItems = function () {
+        for (var i = 0; i < $scope.alertArr.length; i++) {
+            if ($scope.alertArr[i].isChecked) {
+                alerts.remove(i);
+            }
+            alertArr = alerts.getAll();
+        }
+    }
+    $scope.mouseOverUpdate = function() {
+    alert("You can only update one document at a time!");
 }
+    // to do- when update can create new using modal 
+    $scope.updateItem = function (index) {
+        $location.path("/alerts/" + index)
+        // to do-make alert that only one document can be updated at a time
+    }
+    alertArr = alerts.getAll();
 
-)
+})
+    
+    
+
+
+
